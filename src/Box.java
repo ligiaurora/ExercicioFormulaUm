02d76a1a7f7bc9d3c2fb9add896939dd8cdbd608
Ligia Aurora <ligiaurora@gmail.com>
2025-03-30 19:55:29 -0300

    Atualização das classes Carro e Box

--- src/Box.java
+++ src/Box.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+public class Box implements Runnable {
+    private Carro carro;
 
-public class Box {
+    public Box(Carro carro) {
+        this.carro = carro;
+    }
+
+    @Override
+    public void run() {
+        int velocidade = carro.getVelocidadeAtual();
+
+        System.out.println(carro.getNome() + " entrou no box com a velocidade igual de:" + velocidade + " km/h.");
+
+        if (velocidade > 60) {
+            penalidade();
+        } else {
+            passarBox();
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void passarBox() {
+        System.out.println(carro.getNome() + " está no box");
+        pausa(2000);
+        System.out.println(carro.getNome() + " saiu do box.");
+    }
+
+    private void penalidade() {
+        System.out.println(carro.getNome() + " foi penalizado por ultrapassar o limite acima de 60km ao entrar no box");
+    }
+
+    private void pausa(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Carro.java
+++ src/Carro.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
-public class Carro {
+public class Carro implements Runnable {
+    private String nome;
+    private int velocidade;
+    private int velocidadeAtual;
+    private boolean correndo;
+
+    public Carro(String nome) {
+        this.nome = nome;
+        this.velocidade = new Random().nextInt(101) + 100;
+        this.correndo = true;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+
+        while (correndo) {
+            int acao = random.nextInt(5) + 1;
+
+            switch (acao) {
+                case 1 -> correr();
+                case 2 -> pararPorProblema();
+                case 3, 4, 5 -> new Thread(new Box(this)).start();
+            }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void correr() {
+        int variacao = new Random().nextInt(21) - 10;
+        velocidadeAtual = velocidade + variacao;
+        System.out.println(nome + " está correndo a " + velocidadeAtual + " km/h");
+    }
+
+    private void pararPorProblema() {
+        System.out.println(nome + "Fim de prova por problemas técnicos");
+        correndo = false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
+
+    public boolean isCorrendo() {
+        return correndo;
+    }
 }
